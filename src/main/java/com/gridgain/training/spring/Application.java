aea2897f1f27fdd172fb32cc02175157fd95bb83
Scott Stokes <sstokes99@gmail.com>
2025-02-13 07:59:11 -0600

    Update Application.java
    Commented out scanPackages

--- src/main/java/com/gridgain/training/spring/Application.java
+++ src/main/java/com/gridgain/training/spring/Application.java
@@ -3,7 +3,7 @@ package com.gridgain.training.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.gridgain.training.spring", exclude = {IgniteClientAutoConfiguration.class})
+@SpringBootApplication // (scanBasePackages = "com.gridgain.training.spring", exclude = {IgniteClientAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
